bc5bf704fff0053ab1cbd52a3cae4c3ed2d03f47
ROHIT SINGH <rohitsinghajay08@gmail.com>
2022-04-21 10:34:57 +0530

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Pdfreader.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload id="FileUpload1" runat="server" />
+        </div>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Button" />
+        </p>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace Pdfreader
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+
+                FileUpload1.SaveAs(@"C:\temp\" + FileUpload1.FileName);
+                Label1.Text = "File Uploaded: " + FileUpload1.FileName;
+            }
+            else
+            {
+                Label1.Text = "No File Uploaded.";
+            }
+
+        }
+    }
+}
